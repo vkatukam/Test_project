db3b810978d6568d4dfb1ab5ab5c41bdbeff12ec
vkatukam <46891707+vkatukam@users.noreply.github.com>
2020-04-03 11:23:21 +0530

    Added wget file

---
+++ wget.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+repo="http://192.168.189.246:8081/"
+group=$groupId
+artifact=$artifactId
+version=$Version
+
+if [[ $type == "" ]]; then
+  type="war"
+fi
+if [[ $classifier != "" ]]; then
+  classifier="-${classifier}"
+fi
+
+groupIdUrl="${group//.//}"
+filename="${artifact}-${version}${classifier}.${type}"
+
+if [[ ${version} == *"SNAPSHOT"* ]]; then repo_type="snapshots"; else repo_type="releases"; fi
+
+if [[ $repo_type == "releases" ]]
+ then
+ wget --no-check-certificate "${repo}/repository/releases/${groupIdUrl}/${artifact}/${version}/${artifact}-${version}${classifier}.${type}" -O ${filename} -k
+ else
+   versionTimestamped=$(wget -q -O- --no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifact}/${version}/maven-metadata.xml" | grep -m 1 \ | sed -e 's/\(.*\)<\/value>/\1/' | sed -e 's/ //g')
+
+   wget --no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifact}/${version}/${artifact}-${versionTimestamped}${classifier}.${type}" -O ${filename}
+ fi
+ 
