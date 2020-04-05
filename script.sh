7a0d343016b18d09f04fbecb6c6bdeeb60e13375
vkatukam <46891707+vkatukam@users.noreply.github.com>
2020-04-05 20:20:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ if [[ $repo_type == "releases" ]]
  wget --user admin --password 341Zbojw@ --no-check-certificate "${repo}/repository/releases/${groupIdUrl}/${artifact}/${version}/${artifact}-${version}${classifier}.${type}" -O ${filename} -k
  else
    versionTimestamped=$(wget --user admin --password 341Zbojw@ -q -O- --no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifact}/${version}/maven-metadata.xml" | grep -m 1 \ | sed -e 's/\(.*\)<\/value>/\1/' | sed -e 's/ //g')
-
+   echo "$versionTimestamped"
    wget --user admin --password 341Zbojw@ no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifact}/${version}/${artifact}-${versionTimestamped}${classifier}.${type}" -O ${filename}
  fi
  
