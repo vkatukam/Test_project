62f2307d94d49a3e0f54fb7bab59c5edfda1cbf9
vkatukam <46891707+vkatukam@users.noreply.github.com>
2020-04-07 18:11:10 +0530

    modified script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-repo="http://192.168.189.246:8081/"
+repo="http://192.168.189.246:8081"
 group=$groupId
 artifact=$artifactId
 version=$Version
@@ -8,21 +8,15 @@ version=$Version
 if [[ $type == "" ]]; then
   type="war"
 fi
-if [[ $classifier != "" ]]; then
-  classifier="-${classifier}"
-fi
+#if [[ $classifier != "" ]]; then
+ # classifier="-${classifier}"
+#fi
 
 groupIdUrl="${group//.//}"
-filename="${artifact}-${version}${classifier}.${type}"
+filename="${artifact}-${version}.${type}"
 
-if [[ ${version} == *"SNAPSHOT"* ]]; then repo_type="snapshots"; else repo_type="releases"; fi
+wget --user admin --password 341Zbojw@  "${repo}/repository/maven-public/${groupIdUrl}/${artifact}/${version}/maven-metadata.xml"
 
-if [[ $repo_type == "releases" ]]
- then
- wget --user admin --password 341Zbojw@ --no-check-certificate "${repo}/repository/releases/${groupIdUrl}/${artifact}/${version}/${artifact}-${version}${classifier}.${type}" -O ${filename} -k
- else
-   versionTimestamped=$(wget --user admin --password 341Zbojw@ -q -O- --no-check-certificate "${repo}/repository/maven-public/${groupIdUrl}/${artifact}/${version}/maven-metadata.xml" | grep -m 1 "value" | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')
+versionTimestamped=$(grep -m 1 "value" "maven-metadata.xml" | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')
 
-   wget --user admin --password 341Zbojw@ no-check-certificate "${repo}/repository/maven-public/${groupIdUrl}/${artifact}/${version}/${artifact}-${versionTimestamped}.${type}" -O ${filename}
- fi
- 
+   wget --user admin --password 341Zbojw@  "${repo}/repository/maven-public/${groupIdUrl}/${artifact}/${version}/${artifact}-${versionTimestamped}.${type}" -O ${filename}
